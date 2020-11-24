f23e3755caae34a4dc2ccd723fbf2721885e171d
Luiz Faria <lfaria@Luizs-MacBook-Pro.local>
2020-11-24 08:55:04 +0100

    modify mesh to work with LinRange, add docstrings

--- src/meshes.jl
+++ src/meshes.jl
@@ -5,9 +5,18 @@ An abstract mesh structure in dimension `N` with primite data of type `T`.
 """
 abstract type AbstractMesh{N,T} end
 
+"""
+    struct CartesianGrid{N,T} <: AbstractMesh{N,T}
+    
+An `N`-dimensional cartesian grid given as the tensor-product of `N` one-dimensional
+`LinRange{T}` objects. The grid spacing is therefore constant per dimension.
+"""
 struct CartesianGrid{N,T} <: AbstractMesh{N,T}
-    grid1d::NTuple{N,Vector{T}}
+    grid1d::NTuple{N,LinRange{T}}
 end    
+
+grid1d(g::CartesianGrid)     = g.grid1d
+grid1d(g::CartesianGrid,dim) = g.grid1d[dim]
 
 dimension(g::CartesianGrid{N}) where {N} = N
 
@@ -19,9 +28,10 @@ zgrid(g::CartesianGrid) = g.grid1d[3]
 # type if needed
 CartesianGrid(args...) = CartesianGrid(promote(args...))
 
-meshsize(g::CartesianGrid)  = map(x->x[2]-x[1],g.grid1d)
+meshsize(g::CartesianGrid)      = step.(grid1d(g))
+meshsize(g::CartesianGrid,dim)  = step(grid1d(g,dim))
 
-Base.size(g::CartesianGrid) = map(length,g.grid1d)
+Base.size(g::CartesianGrid) = length.(g.grid1d)
 Base.length(g) = prod(size(g))
 
 function Base.getindex(g::CartesianGrid,I) 
