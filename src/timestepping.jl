46b2d16aa627fbd749f6a30143bc585981f83eb7
Luiz M. Faria <maltezfaria@gmail.com>
2020-11-25 15:51:08 +0100

    better name

--- src/timestepping.jl
+++ src/timestepping.jl
@@ -5,12 +5,12 @@ Base.@kwdef struct ForwardEuler{T}
     buffer::T
 end
 
-function evolve!(ϕ,feuler::ForwardEuler,terms,bc,t,Δtᵃ=Inf)
+function evolve!(ϕ,feuler::ForwardEuler,terms,bc,t,Δtmax=Inf)
     buffer      = feuler.buffer
     fill!(values(buffer),0)
     buffer, Δtˢ = compute_terms!(buffer,terms,ϕ,bc)    
     α = feuler.cfl
-    Δt = min(Δtᵃ,α*Δtˢ)
+    Δt = min(Δtmax,α*Δtˢ)
     # buffer now holds the rhs. Update ϕ in-place
     axpy!(-Δt,buffer.vals,ϕ.vals) # ϕ = ϕ - dt*buffer
     return ϕ,t+Δt
@@ -21,14 +21,14 @@ Base.@kwdef struct RK2{T}
     buffers::NTuple{2,T}
 end
 
-function evolve!(ϕ,integ::RK2,terms,bc,t,Δtᵃ=Inf)
+function evolve!(ϕ,integ::RK2,terms,bc,t,Δtmax=Inf)
     α = integ.cfl    
     buffer1,buffer2 = integ.buffers[1],integ.buffers[2]
     fill!(values(buffer1),0)
     fill!(values(buffer2),0)
     #
     buffer1, Δtˢ = compute_terms!(buffer1,terms,ϕ,bc) 
-    Δt = min(Δtᵃ,α*Δtˢ) 
+    Δt = min(Δtmax,α*Δtˢ) 
     axpy!(-Δt/2,buffer1.vals,ϕ.vals) # ϕ = ϕ - dt/2*buffer1
     #
     @. buffer1.vals = ϕ.vals + Δt * buffer1.vals   
