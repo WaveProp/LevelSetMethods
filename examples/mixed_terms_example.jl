6fde408b89f8be3ac550dc10965f98e9917cd368
Luiz M. Faria <maltezfaria@gmail.com>
2020-11-26 17:13:58 +0100

    add advection+normal motion example

---
+++ examples/mixed_terms_example.jl
@@ -0,0 +1,38 @@
+using Test
+using LevelSetMethods
+using LinearAlgebra
+using Plots
+
+nx,ny = 100,100
+x     = LinRange(-1,1,nx)
+y     = LinRange(-1,1,ny)
+hx,hy = step(x),step(y)
+grid = CartesianGrid(x,y)
+bc    = PeriodicBC(2)
+ϕ    = LevelSet(grid,bc) do (x,y)
+    1.0
+end
+add_circle!(ϕ,SVector(0.5,0.0),0.25)
+add_circle!(ϕ,SVector(-0.5,0.0),0.25)
+add_rectangle!(ϕ,SVector(0.0,0.0),SVector(1.0,0.1))
+plot(ϕ)
+v     = MeshField(grid) do (x,y)
+    -0.1
+end
+𝐮     = MeshField(grid) do (x,y)
+    SVector(-y,x)
+end   
+term1  = NormalAdvectionTerm(v)
+term2  = AdvectionTerm(𝐮)
+terms = (term1,term2)
+b = zero(ϕ)
+integrator = ForwardEuler(0.5)
+eq = LevelSetEquation(;terms,integrator,state=ϕ,t=0,buffer=b)
+
+dt = 0.01
+anim = @animate for n ∈ 0:100
+    tf = dt*n    
+    integrate!(eq,tf)    
+    plot(eq,linecolor=:black)    
+end
+gif(anim, "test.gif")
